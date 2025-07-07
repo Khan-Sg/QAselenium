d5048158a0fe63d755dd700b9e80a4b8af3d7754
Khan-Sg <abuzar.ahamed.khan@gmail.com>
2025-07-07 16:12:34 +0530

    programs

---
+++ ActiTime-Automation/src/test/java/com/sgtesting/p3/P1Sumofnumber1to25.java
@@ -0,0 +1,11 @@
+package com.sgtesting.p3;
+//print all sum
+public class P1Sumofnumber1to25{
+    public static void main(String[] args) {
+        int sum=0;
+        for(int i=1;i<=25;i++){
+            sum=sum+i;
+
+        }System.out.println(sum);
+    }
+}
