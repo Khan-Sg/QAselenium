8bfa87028e01dccc7740ef98b1657b89081a5b8c
Khan-Sg <abuzar.ahamed.khan@gmail.com>
2025-11-27 14:18:20 +0530

    All links program

--- ActiTime-Automation/src/test/java/com/sgtesting/stepdefinations/BrokenLinks.java
+++ ActiTime-Automation/src/test/java/com/sgtesting/stepdefinations/BrokenLinks.java
@@ -4,16 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.ResponseCodec;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.Duration;
 import java.util.List;
 
-public class Delete {
+public class BrokenLinks {
     public static void main(String[] args) {
        WebDriver driver=new ChromeDriver();
        driver.manage().window().maximize();
